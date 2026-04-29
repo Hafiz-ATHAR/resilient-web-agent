184ff0b64253eb982d940b84113ce288daa0a4ca
Hafiz Athar <hafiz.athar@tribetech.de>
2026-04-29 08:03:20 +0000

    Added request_id middleware.

---
+++ app/middleware/request_id.py
@@ -0,0 +1,22 @@
+import uuid
+
+import structlog
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.types import ASGIApp
+
+
+class RequestIdMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, header_name: str = "X-Request-ID") -> None:
+        super().__init__(app)
+        self.header_name = header_name
+
+    async def dispatch(self, request: Request, call_next):
+        request_id = request.headers.get(self.header_name) or str(uuid.uuid4())
+        structlog.contextvars.bind_contextvars(request_id=request_id)
+        try:
+            response = await call_next(request)
+        finally:
+            structlog.contextvars.unbind_contextvars("request_id")
+        response.headers[self.header_name] = request_id
+        return response
